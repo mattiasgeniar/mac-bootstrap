0d6a3ff11957b4673d28cc35f80c2e89b62bcbd2
Mattias Geniar <m@ttias.be>
2017-02-09 12:03:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+== Usage
+
+One script to rule them all!
+
+```
+$ bash install.sh
+```
+
+== Credits
+
+Heavily stolen from Dries Vints' dotfiles repo: https://github.com/driesvints/dotfiles/blob/master/install.sh
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle --file=conf.d/Brewfile
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
